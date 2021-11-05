082d46d4aa1a9c572668a04c99dc601f1af05960
AboimovaON <85744610+AboimovaON@users.noreply.github.com>
2021-11-05 21:43:46 +0500

    Четвертое задание

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+
+package com.company;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        String[] array = {"Ольга", "Алексей", "Иван", "Петр", "Алексей", "Иван", "Петр", "Алексей", "Алексей", "Иван", "Петр", "Алексей", "Иван", "Петр"};
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        for (String name : array) {
+            if (hashMap.get(name) == null) {
+                hashMap.put(name, 1);
+            } else {
+                hashMap.put(name, hashMap.get(name) + 1);
+            }
+        }
+        System.out.println(hashMap);
+
+        Phonebook phonebook = new Phonebook();
+        phonebook.add("olga", " 4545634131");
+        phonebook.add("Igor", " 4545634132");
+        phonebook.add("Vasyl", " 4545634133");
+        phonebook.add("olga", " 4545634134");
+        phonebook.add("Igor", " 4545634135");
+        phonebook.add("olga", " 4545634136");
+
+
+        phonebook.get("olga");
+        phonebook.get("Igor");
+        phonebook.get("Vasyl");
+
+    }
+
+}
---
+++ src/com/company/Phonebook.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Phonebook {
+
+        HashMap<String, ArrayList<String>> namesAndNumbers;
+
+        public Phonebook(){;
+            namesAndNumbers = new HashMap<>();
+        }
+        public void add(String name, String number){
+            ArrayList<String> newNumber = new ArrayList<>();
+            if (namesAndNumbers.get(name) == null) {
+                newNumber.add(number);
+                namesAndNumbers.put(name, newNumber);
+            }
+            else {namesAndNumbers.get(name).add(number);
+            }
+
+        }
+        public void get(String name){
+            System.out.println(name + namesAndNumbers.get(name));
+        }
+}
